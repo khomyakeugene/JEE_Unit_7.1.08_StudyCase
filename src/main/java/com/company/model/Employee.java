6c028c38dfe82ab7d302edfa4c0c5770bb1db480
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-04 14:42:42 +0300

    JEE Unit 7.1.08 StudyCase

--- src/main/java/com/company/model/Employee.java
+++ src/main/java/com/company/model/Employee.java
@@ -1,11 +1,30 @@
 package com.company.model;
 
+import javax.persistence.*;
+
 /**
  * Created by Yevhen on 04.06.2016.
  */
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "surname")
     private String surname;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Enumerated(EnumType.ORDINAL)
+    private Position position;
+
+
+    private Float salary;
 
 }
