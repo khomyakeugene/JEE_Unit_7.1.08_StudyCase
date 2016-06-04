f55f225794781904725285713b286b3fdc2c00ce
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-04 12:58:50 +0300

    JEE Unit 7.1.08 StudyCase

--- src/main/java/com/company/model/Employee.java
+++ src/main/java/com/company/model/Employee.java
@@ -4,4 +4,8 @@ package com.company.model;
  * Created by Yevhen on 04.06.2016.
  */
 public class Employee {
+    private String name;
+    private String surname;
+    private String phoneNumber;
+
 }
