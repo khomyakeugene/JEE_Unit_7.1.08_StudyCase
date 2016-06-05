7eda0bd7807b1814339b9e06d0f153654d622646
khomyakeugene <khomyak_eugene@yahoo.com>
2016-06-05 13:13:13 +0300

    JEE Unit 7.1.08 StudyCase

--- src/main/java/com/company/model/Employee.java
+++ src/main/java/com/company/model/Employee.java
@@ -28,4 +28,51 @@ public class Employee {
     @Column(name = "salary")
     private Float salary;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Float salary) {
+        this.salary = salary;
+    }
 }
--- src/main/java/com/company/model/controllers/EmployeeController.java
+++ src/main/java/com/company/model/controllers/EmployeeController.java
@@ -1,13 +1,26 @@
 package com.company.model.controllers;
 
 import com.company.model.Employee;
+import com.company.model.EmployeeDao;
+import com.company.model.Position;
 
 /**
  * Created by Yevhen on 05.06.2016.
  */
 public class EmployeeController {
 
+    private EmployeeDao employeeDao;
+
     public void createEmployee() {
         Employee employee = new Employee();
+
+        employee.setId(1L);
+        employee.setName("John");
+        employee.setSurname("Smith");
+        employee.setPosition(Position.WAITER);
+        employee.setPhoneNumber("555-55-55");
+        employee.setSalary(25000.0F);
+
+        employeeDao.save(employee);
     }
 }
